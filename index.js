19aa5ff3cd6b1a459f7250b8f007dc3ed7161165
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-03-12 07:53:30 +0300

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+return drivers.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+    };
+    console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+    const createFareMultiplier = function(integer){
+        return function(fare) {
+            return integer * fare
+        }
+    }
+
+    const fareDoubler = createFareMultiplier(2);
+
+    const fareTripler = createFareMultiplier(3);
+       
+    const selectDifferentDrivers = function(drivers, returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers)
+    }
